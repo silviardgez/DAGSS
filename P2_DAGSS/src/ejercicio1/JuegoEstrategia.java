8e7de5fc4dfeaa7d31b4057bba8f00d8ae32fe6a
Silvia Rodríguez <silviardguez@gmail.com>
2017-10-10 12:00:58 +0200

    Jugador puede escoger equipo

--- P2_DAGSS/src/ejercicio1/JuegoEstrategia.java
+++ P2_DAGSS/src/ejercicio1/JuegoEstrategia.java
@@ -1,14 +1,30 @@
 package ejercicio1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class JuegoEstrategia {
 
-	public static void main(String[] args) {
-		Jugador maquina = new JugadorMaquina(new HarkonenSoldadosFactory());
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String line;
+		Jugador humano = null;
+		Jugador maquina = null;
 		
-		//podriamos pedir el equipo al jugador
-		Jugador humano = new JugadorHumano(new AtreidesSoldadosFactory());
-				
-		while(true){
+		// Pedimos el equipo al jugador
+		System.out.println("¿Qué equipo desea? [a=atreides, h=harkonen");
+		line = in.readLine();
+		if (line.startsWith("a")) {
+			humano = new JugadorHumano(new AtreidesSoldadosFactory());
+			maquina = new JugadorMaquina(new HarkonenSoldadosFactory());
+		} else if (line.startsWith("h")) {
+			humano = new JugadorHumano(new HarkonenSoldadosFactory());
+			maquina = new JugadorMaquina(new AtreidesSoldadosFactory());
+		}
+
+		while (true) {
 			System.out.println("JUEGA MAQUINA");
 			maquina.jugar();
 			System.out.println("JUEGA HUMANO");
